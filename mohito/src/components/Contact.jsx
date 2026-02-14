e4fe3b06410fce3461fda2a6fad4c91a51136a28
John Francis <johnmosima7@gmail.com>
2026-02-14 17:30:26 +0300

    Implement Contact Section

--- mohito/src/components/Contact.jsx
+++ mohito/src/components/Contact.jsx
@@ -1,6 +1,67 @@
+import { useGSAP } from "@gsap/react"
+import { openingHours, socials } from "../constants"
+import { SplitText } from "gsap/all"
+import gsap from "gsap"
+
 const Contact = () => {
+
+    useGSAP(() => {
+        const titleSplit = SplitText.create("#contact h2", { type: "words" })
+
+        const timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#contact",
+                start: "top center"
+            },
+            ease: "power1.inOut"
+        })
+
+        timeline
+        .from(titleSplit.words, { opacity: 0, yPercent: 100, stagger: 0.02 })
+        .from("#contact h3, #contact p", { opacity: 0, yPercent: 100, stagger: 0.02 })
+        .to("#f-right-leaf", { duration: 1, y: '-50', ease: "power1.inOut" })
+        .to("#f-left-leaf", { duration: 1, y: '-50', ease: "power1.inOut" }, '<')
+    })
   return (
-    <div>Contact</div>
+    <footer id="contact">
+        <img src="/images/footer-right-leaf.png" alt="leaf-right" id="f-right-leaf" />
+        <img src="/images/footer-left-leaf.png" alt="leaf-left" id="f-left-leaf" />
+
+        <div className="content">
+            <h2>Where to Find Us</h2>
+            <div>
+                <h3>Visit Our Bar</h3>
+                <p>123 Cocktail Avenue, Mixology City, 45678</p>
+            </div>
+
+            <div>
+                <h3>Contact Us</h3>
+                <p>Phone: (+254) 456-7890</p>
+                <p>info@mojitobars.com</p>
+            </div>
+
+            <div>
+                <h3>Open Every Day</h3>
+                {openingHours.map((time) => (
+                    <p key={time.day}>
+                        {time.day} : {time.time}
+                    </p>
+                ))}
+            </div>
+
+            <div>
+                <h3>Socials</h3>
+
+                <div className="flex-center gap-5">
+                    {socials.map((social) => (
+                        <a key={social.name} href={social.url} target="_blank" rel="noopener noreferrer" aria-label={social.name}>
+                            <img src={social.icon} alt={`${social.name}-icon`} />
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </div>
+    </footer>
   )
 }
 
