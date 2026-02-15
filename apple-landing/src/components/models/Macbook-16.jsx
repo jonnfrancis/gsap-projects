f9574eba94d85e034ff512afdbcfe3edcddf4ef8
John Francis <johnmosima7@gmail.com>
2026-02-15 16:42:14 +0300

    Fix material override

--- apple-landing/src/components/models/Macbook-16.jsx
+++ apple-landing/src/components/models/Macbook-16.jsx
@@ -10,7 +10,7 @@ Title: macbook pro M3 16 inch 2024
 
 import { useGLTF, useTexture } from '@react-three/drei'
 import { noChangeParts } from '../../constants';
-import { Color } from 'three';
+import { Color, SRGBColorSpace } from 'three';
 import { useEffect } from 'react';
 import useMacbookStore from '../../store';
 
@@ -19,6 +19,8 @@ export default function MacbookModel16(props) {
   const { color } = useMacbookStore();
 
   const texture = useTexture('/screen.png')
+  texture.colorSpace = SRGBColorSpace;
+  texture.needsUpdate = true;
 
   useEffect(() => {
     scene.traverse((child) => {
@@ -48,7 +50,7 @@ export default function MacbookModel16(props) {
       <mesh geometry={nodes.Object_82.geometry} material={materials.gMtYExgrEUqPfln} rotation={[Math.PI / 2, 0, 0]} />
       <mesh geometry={nodes.Object_96.geometry} material={materials.PaletteMaterial003} rotation={[Math.PI / 2, 0, 0]} />
       <mesh geometry={nodes.Object_107.geometry} material={materials.JvMFZolVCdpPqjj} rotation={[Math.PI / 2, 0, 0]} />
-      <mesh geometry={nodes.Object_123.geometry} material={materials.sfCQkHOWyrsLmor} rotation={[Math.PI / 2, 0, 0]}>
+      <mesh geometry={nodes.Object_123.geometry} rotation={[Math.PI / 2, 0, 0]}>
         <meshBasicMaterial map={texture} />
       </mesh>
       <mesh geometry={nodes.Object_127.geometry} material={materials.ZCDwChwkbBfITSW} rotation={[Math.PI / 2, 0, 0]} />
